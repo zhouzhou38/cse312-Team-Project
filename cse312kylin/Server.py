d62e2ddc8e915ae7538672addfceaf0697e0f659
zhou zhou <zzhou38@buffalo.edu>
2022-05-13 21:37:04 -0400

    0513

--- cse312kylin/Server.py
+++ cse312kylin/Server.py
@@ -21,20 +21,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         sys.stdout.flush()
 
         if data_arr[0] == b'GET ' and b' HTTP' == data_arr[1]:
-            # load home page localhost:5454
-            # f = open("HTMLtemplates/new_homepage.html", 'rb')
-            # content = f.read()
-            # start_idx = content.find(b'{{start_moment}}')
-            # end_idx = content.find(b'{{end_moment}}')
-            # print('start_idx :', start_idx)
-            # sys.stdout.flush()
-            # if moment_info.find({}) is None:
-            #     print('nothing post here')
-            #     sys.stdout.flush()
-            #     empty_moment = b''
-            #     content = content[:start_idx] + empty_moment + content[end_idx + len(b'{{end_moment}}'):]
-
-
             f = open("HTMLtemplates/SignIn.html",'rb')
             content = f.read()
             header = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-length:"
@@ -71,10 +57,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             if information is None:
                 toolBox.inserUsertoDB(userName, password)
                 print('hello')
-                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://localhost:8080/\r\n\r\n"
+                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://zhouchating.com:8080/\r\n\r\n"
                 self.request.sendall(header)
             else:
-                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://localhost:8080/Signup/?error=username\r\n\r\n"
+                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://zhouchating.com:8080/Signup/?error=username\r\n\r\n"
                 self.request.sendall(header)
             '''
             1. verify username is exist on our database
@@ -96,7 +82,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             if information is not None:
                 pwd = information['Password']
                 if bcrypt.checkpw(password, pwd):
-                    # localhost:5454/?name=username
                     mytoken = secrets.token_hex(16).encode()
                     tokenhashed = bcrypt.hashpw(mytoken, bcrypt.gensalt())
                     user_list.update_one({"UserName": userName}, {'$set': {'cookie': tokenhashed}})
@@ -113,20 +98,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         content = content[:start_idx] + empty_moment + content[end_idx + len(b'{{end_moment}}'):]
                     self.request.sendall(toolBox.general_sender("HTMLtemplates/new_homepage.html", content))
                 else:
-                    header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://localhost:8080/?error=password\r\n\r\n"
+                    header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://zhouchating.com:8080/?error=password\r\n\r\n"
                     self.request.sendall(header)
             else:
-                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://localhost:8080/?error=username\r\n\r\n"
+                header = b"HTTP/1.1 301 Permanent Redirect\r\nContent-Length:0\r\nLocation:http://zhouchating.com:8080/?error=username\r\n\r\n"
                 self.request.sendall(header)
 
-        elif data_arr[0] == b'GET ' and data_arr[2] == b'friend_request_box HTTP':
-            # localhost:5454/kylin/friend_request_box
-            # localhost:5454/zhou/friend_request_box
-            username = data_arr[1].split(b' ')[0]
-            '''
-            load friend_request_box base on username
-            return database from username, transfer to html
-            '''
         elif data_arr[0] == b'GET ' and data_arr[1] == b'style.css HTTP':
             print('starting request css')
             sys.stdout.flush()
@@ -134,11 +111,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
         elif data_arr[0] == b'GET ' and b'sakura.jpg HTTP' in data_arr[1]:
             self.request.sendall(toolBox.image_sender('sakura.jpg'))
-
-        elif data_arr[0] == b'GET ' and data_arr[1] == b'direct_message_box HTTP':
-            pass
-        elif data_arr[0] == b'GET ' and data_arr[1] == b'moment_box HTTP':
-            pass
         else:
             self.request.sendall(toolBox.function_404('This does not exist!'))
 
